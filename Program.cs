1232477413aa69d4b51b10ff85398611ed3f1f08
Sidswipe <sidswipexyz@gmail.com>
2024-02-24 19:36:03 +0100

    Do-while Statements

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//Initiates variables to hold the Live Scores for both the Hero and the Monster
+int heroLiveScore = 10;
+int monsterLiveScore = 10;
+
+Random live = new Random(); //Initialize a new Random integer generator
+
+do
+{  
+
+    int subLive = live.Next(1,11); //Generates random live score subtractor for Monsters attacks
+    monsterLiveScore -= subLive;
+    Console.WriteLine($"Monster was damaged and lost {subLive} and now has {monsterLiveScore} health");
+
+if (monsterLiveScore <= 0) continue; //The continue keyword tells the compiler to jump to the last line of code block and evaluate the WHILE boolean, if its still TRUE then it continues the line 16
+
+subLive = live.Next(1, 11); //Generates random live score subtractor for Monsters attacks
+heroLiveScore -= subLive;
+    Console.WriteLine($"Hero was damaged and lost {subLive} and now has {heroLiveScore} health");
+
+} while (monsterLiveScore > 0 && heroLiveScore > 0);
+
+Console.WriteLine(monsterLiveScore > heroLiveScore ? "Monster wins" : "Hero wins"); //Tenary conditions to print winner
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# .NET Role playing game battle challenge
+ A hero and a monster start with the same health score. During the hero's turn to attack, the logic will generate a random value which will be subtracted from the monster's health. If the monster's health is greater than zero, they'll take their turn and attack the hero. As long as both the hero and the monster have health greater than zero, the battle will resume. <br>
+
+Used a do-while statement. <br>
+The hero and the monster will start with 10 health points. <br>
+All attacks will be a value between 1 and 10. <br>
+The hero will attack first. <br>
+Prints the amount of health the monster lost and their remaining health. <br>
+If the monster's health is greater than 0, it can attack the hero. <br>
+Prints the amount of health the hero lost and their remaining health. <br>
+Continue this sequence of attacking until either the monster's health or hero's health is zero or less. <br>
+Print the winner. <br>
+
+## DO-WHILE STATEMENTS
